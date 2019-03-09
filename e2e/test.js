56a4a6f124b389c3a51e5ab19f613dc4328c1942
Jim McMahon <jim@madbongo.net>
2019-03-09 11:57:50 -0800

    Make e2e/wdio test use .env

--- e2e/test.js
+++ e2e/test.js
@@ -2,7 +2,7 @@ const expect = require('chai').expect;
 
 describe('Shopping cart', () => {
   it('should add a product to cart and remove it', () => {
-    browser.url('https://react-shopping-cart-67954.firebaseapp.com/');
+    browser.url('process.env.REACT_APP_APP_URL');
     browser.waitForText('.shelf-item');
 
     /* Open float cart */
